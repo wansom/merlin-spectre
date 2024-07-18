2a1f5ae4c2a9250a9d7cce93a08a8ba9f8404a48
wansom <ochiengwarren10@gmail.com>
2024-07-18 12:43:47 +0300

    add use case section

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,139 @@
+'use client'
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const useCases=[
+    {
+      title:'Smart Agriculture',
+      subtitle:"By leveraging AI and IoT, we create precision farming tools that optimise resource use,enhance crop yields, and promote sustainable farming practices. Our solutions include soil moisture sensors, drone-based monitoring, and AI-driven crop health analysis"
+    },
+    {
+      title:'ForeBot',
+      subtitle:'Our flagship IoT device monitors forest fires, logging activities, and atmospheric conditions to provide real-time data for preventive measures. We develop IoT sensors and AI algorithms to monitor environmental conditions, predict weather patterns, and provide real-time data to help communities prepare for and mitigate the impacts of climate change'
+    },
+    {
+      title:'Education Technology',
+      subtitle:'We believe in the power of education to transform lives. Our EdTech solutions use AI to personalise learning experiences, improve access to quality education, and provide tools for remote and underserved communities'
+    },
+    {
+      title:'Security',
+      subtitle:'We streamline security operations using predictive analytics allowing for proactive security measures and quicker identification of potential security breaches.'
+    },
+    {
+      title:'Renewable Energy Management',
+      subtitle:'Our energy solutions focus on harnessing renewable resources and improving energy efficiency. We offer smart grids, solar energy management systems, and AI-driven analytics to optimise energy consumption and distribution. Ensure targeted outreach and sanitation promotion that enhance sustainable water and sanitation practices for all.'
+    },
+    {
+      title:'WASH',
+      subtitle:'Ensure targeted outreach and sanitation promotion that enhance sustainable water and sanitation practices for all.'
+    }
+  ]
+  const [hoveredIndex, setHoveredIndex] = useState(0);
+  return (<main>
+    <div className="min-h-screen  hero bg-cover bg-top">
+      <div className="h-[120px] bg-primary">
+
+      </div>
+<section className=" h-screen flex flex-col gap-5 items-center justify-center container mx-auto px-5 lg:px-28 py-24">
+  <h1 className=" text-navy-blue text-[28px] font-light text-center font-serif lg:max-w-[85%]">
+  Cutting-edge AI and IoT products/services for real-world applications
+Reliable Services Incorporating Data As A Service (DAAS)<br/>
+<span className="text-primary">
+Unlock Growth with AIoT, Data, and Expert Consulting
+Research to Ideation to Product Development
+</span>
+
+  </h1>
+  <div className="flex gap-8">
+    <button className="text-primary border-[2px] border-solid bg-white border-[#1A9988] flex items-center px-4 py-2 rounded-lg">Learn More
+
+    </button>
+    <button className="text-white bg-primary border-[2px] border-solid border-[#1A9988] flex items-center px-4 py-2 rounded-lg">Contact Us
+
+</button>
+
+  </div>
+
+</section>
+<section className="container mx-auto px-5 lg:px-24 lg:mt-48 space-y-4 py-10 ">
+  <h2 className=" text-xl font-semibold text-black text-center">Research, Development and Consultancy (RDC) for Agriculture,
+Forestry, Education, Security, Energy, Water, Sanitation and
+Hygiene (WASH)</h2>
+  <div className="flex justify-between items-center flex-col md:flex-row gap-4">
+    <div className="flex gap-4 items-center">
+      <div className="border-primary rounded-lg p-2 text-primary"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-10 w-10">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+</svg>
+</div>
+<div>
+  <h3 className="font-semibold text-lg">
+    Research
+  </h3>
+  <p className="text-md">Empowering Businesses with Next-Gen Technology</p>
+</div>
+
+    </div>
+    <div className="flex gap-4 items-center">
+      <div className="border-primary rounded-lg p-2 text-primary"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-10 w-10">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+</svg>
+</div>
+<div>
+  <h3 className="font-semibold text-lg">
+    Development
+  </h3>
+  <p className="text-md">Your AIoT and Data Partner for the Future</p>
+</div>
+
+    </div>
+    <div className="flex gap-4 items-center">
+      <div className="border-primary rounded-lg p-2 text-primary"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-10 w-10">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+</svg>
+</div>
+<div>
+  <h3 className="font-semibold text-lg">
+  Consultancy 
+  </h3>
+  <p className="text-md">Unlock Growth with AIoT, Data, and Expert Consulting.</p>
+</div>
+
+    </div>
+
+
+  </div>
+
+</section>
+    </div>
+    <section className=" container mx-auto px-5 lg:px-24 py-12 bg-[#f1f7f6] ">
+      <h2 className="text-primary text-2xl font-semibold text-center">
+        Use Cases
+      </h2>
+      <div className="flex flex-col md:flex-row  gap-6">
+      <div className="md:basis-2/3">
+      {
+        useCases.map((i,index)=>(
+          <div className="bg-white shadow-lg rounded-lg p-4 space-y-3 my-4" key={index} onMouseEnter={() => setHoveredIndex(index)}
+          onMouseLeave={() => setHoveredIndex(null)}>
+          <h3 className="text-primary font-bold text-lg">{i.title}</h3>
+          <p className={`text-black text-md ${ hoveredIndex !== index ? 'hidden' : 'block'}`}>{i.subtitle}</p>
+    
         </div>
+        ))
+      }
+
+
+
+    </div>
+    <div className="md:basis-1/3">
+    <img src="/about.png" className="rounded-lg"/>
+    </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </section>
+  </main>
+    
   );
 }
